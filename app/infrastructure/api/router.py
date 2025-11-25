7194804c6011985c26673c5bee50d4b3d2084496
Sebastian Ilbay <atlantiscityplay@gmail.com>
2025-11-24 22:16:04 -0500

    Update router.py

--- app/infrastructure/api/router.py
+++ app/infrastructure/api/router.py
@@ -37,91 +37,91 @@ router = APIRouter()
 
 router.include_router(
     clientes_web.router,
-    prefix="/web/clientes",
+    prefix="/web",
     tags=["web_clientes"]
 )
 
 router.include_router(
     modulos_web.router,
-    prefix="/web/modulos",
+    prefix="/web",
     tags=["web_modulos"]
 )
 
 router.include_router(
     empresas_web.router,
-    prefix="/web/empresas",
+    prefix="/web",
     tags=["web_empresas"]
 )
 
 router.include_router(
     gimnasios_web.router,
-    prefix="/web/gimnasios",
+    prefix="/web",
     tags=["web_gimnasios"]
 )
 
 router.include_router(
     tipos_empleado_web.router,
-    prefix="/web/tipos_empleado",
+    prefix="/web",
     tags=["web_tipos_empleado"]
 )
 
 router.include_router(
     empleados_web.router,
-    prefix="/web/empleados",
+    prefix="/web",
     tags=["web_empleados"]
 )
 
 router.include_router(
     horarios_gimnasio_web.router,
-    prefix="/web/horarios_gimnasio",
+    prefix="/web",
     tags=["web_horarios_gimnasio"]
 )
 
 router.include_router(
     precio_membresia_web.router,
-    prefix="/web/precio_membresia",
+    prefix="/web",
     tags=["web_precio_membresia"]
 )
 
 router.include_router(
     estados_pago_web.router,
-    prefix="/web/estados_pago",
+    prefix="/web",
     tags=["web_estados_pago"]
 )
 
 router.include_router(
     metodo_pago_web.router,
-    prefix="/web/metodo_pago",
+    prefix="/web",
     tags=["web_metodo_pago"]
 )
 
 router.include_router(
     membresias_web.router,
-    prefix="/web/membresias",
+    prefix="/web",
     tags=["web_membresias"]
 )
 
 router.include_router(
     empleados_asignacion_web.router,
-    prefix="/web/empleados_asignacion",
+    prefix="/web",
     tags=["web_empleados_asignacion"]
 )
 
 router.include_router(
     asistencias_web.router,
-    prefix="/web/asistencias",
+    prefix="/web",
     tags=["web_asistencias"]
 )
 
 router.include_router(
     medidas_corporales_web.router,
-    prefix="/web/medidas_corporales",
+    prefix="/web",
     tags=["web_medidas_corporales"]
 )
 
 router.include_router(
     empleados_dto_web.router,
-    prefix="/web/empleados_dto",
+    prefix="/web",
     tags=["web_empleados_dto"]
 )
 
@@ -131,6 +131,6 @@ router.include_router(
 
 router.include_router(
     clientes_movil.router,
-    prefix="/movil/clientes",
+    prefix="/movil",
     tags=["movil_clientes"]
 )
